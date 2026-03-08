b1c7b50e9fcd38f519fbb50a203ebfb011fa3cc5
Dmitriy Kolesnikov <dmitriy.kolesnikov2007@mail.ru>
2026-03-08 17:15:27 +0300

    Number 1

---
+++ Homework3/HW3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    public class HW3
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        // #1.
+        public static string[] AddSpace(string[] mass)
+        {
+            if (mass == null)
+                throw new ArgumentNullException("Null");;
+            foreach (string s in mass)
+            {
+                if (s.Length % 2 == 0)
+                    throw new ArgumentException("Длина строк должна быть нечётной");
+            }
+
+            int maxLen = mass.Max(s => s.Length);
+            string[] result = new string[mass.Length];
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                int space = (maxLen - mass[i].Length) / 2;
+                result[i] = new string(' ', space) + mass[i] + new string(' ', space);
+            }
+            return result;
+        }
+
+
+
+
+    }
+}
---
+++ TestHomeWork3/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Homework3;
+using static Homework3.HW3;
+
+namespace TestHomeWork3
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+        /// Test #1
+        [Test]
+        public void TestAddSpace1()
+        {
+            Assert.That(() => AddSpace(new string[] { "abc", "abcde", "a", "abcdefg", "abcdefghi" }), Is.EqualTo(new string[] { "   abc   ", "  abcde  ", "    a    ", " abcdefg ", "abcdefghi" }));
+        }
+        [Test]
+        public void TestAddSpace2()
+        {
+            Assert.Throws<ArgumentNullException>(() => AddSpace(null));
+        }
+        [Test]
+        public void TestAddSpace3()
+        {
+            Assert.Throws<ArgumentException>(() => AddSpace(new string[] { "abama", "ro", "jpg" }));
+        }
+
+
+        /// Test #2.
+        
+
+    }
+}
